8a096694c1013f5d208b8f4b8a1dd51b743bc6ff
maamirkh <maamirkhan1983@gmail.com>
2024-04-04 00:35:56 +0500

    updated

---
+++ app.ts
@@ -0,0 +1,9 @@
+#! usr/bin/env node
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+alien_color = "red";
+if (alien_color == "green") {
+    // No output because the condition is false
+}
